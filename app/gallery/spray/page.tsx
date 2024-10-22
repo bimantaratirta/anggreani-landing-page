e37da45e7046157c180dbc07dc6f28acbb0a20a8
bimantaratirta <bimantarats@gmail.com>
2024-10-22 16:48:19 +0700

    feat: restrict stamp and spray page

--- app/gallery/spray/page.tsx
+++ app/gallery/spray/page.tsx
@@ -5,6 +5,7 @@ import Dummy from "@/public/carousel1.jpg";
 import Dummy2 from "@/public/carousel2.jpeg";
 import Image from "next/image";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { useAuth } from "@/context/AuthContext";
 
 const images = [
   { id: 1, title: "Foto 1", src: Dummy },
@@ -28,6 +29,25 @@ const images = [
 
 const Spray: React.FC = () => {
   const [selectedImage, setSelectedImage] = useState<number | null>(null);
+  const crypto = require("crypto");
+  const auth = useAuth();
+  const [form, setForm] = useState<{ pw: string; error?: string }>({ pw: "" });
+  const access =
+    "615a76a1c81adc95c890a5b70f548a0a5a39be11c82641270c5c529b9f076521";
+
+  function handleSubmit() {
+    if (form.pw === access) {
+      auth.login();
+    } else {
+      setForm((old) => ({ ...old, error: "Wrong Password!" }));
+    }
+  }
+
+  function handleOnChange(pw: string) {
+    setForm({
+      pw: crypto.createHash("sha256").update(pw).digest("hex"),
+    });
+  }
 
   const handleImageClick = (index: number) => {
     setSelectedImage(index);
@@ -48,6 +68,40 @@ const Spray: React.FC = () => {
   const closeModal = () => {
     setSelectedImage(null);
   };
+
+  if (!auth.isLogin) {
+    return (
+      <div className="flex items-center justify-center min-h-screen relative">
+        <div className="max-w-sm w-full mx-auto absolute p-8 bg-gray-100 rounded-md">
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              You need to insert password to see our contents!
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              onChange={(e) => handleOnChange(e.target.value)}
+              required
+            />
+            <label htmlFor="" className="text-red-500 text-sm">
+              {form.error}
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="p-6">
